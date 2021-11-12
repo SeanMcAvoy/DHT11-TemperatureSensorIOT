d927171341bc3f7a0ee1435cc9a069fea5a82d57
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-11-12 17:11:22 +0000

    Code is working getting readings NOTE: lose wire keeps breaking Connection

--- DHT11Sensor.py
+++ DHT11Sensor.py
@@ -4,14 +4,13 @@ import time
 
 # sensor type
 DHT_SENSOR = Adafruit_DHT.DHT11
-
 # GPIO pin DHT11 output is going too
-GPIO_PIN = 17
+GPIO_PIN = 4
 
 
 def main():
     while True:
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, GPIO_PIN)
+        humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, GPIO_PIN)
         if humidity is not None and temperature is not None:
             print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
         else:
