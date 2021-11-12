a9ea9152c0270e05f7c2353f315d8cccbe6569d2
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-11-12 16:36:57 +0000

    Sensor and GPIO pin coded and code to read the temperature ready

--- DHT11Sensor.py
+++ DHT11Sensor.py
@@ -1,2 +1,27 @@
+#imports Adafruit_DHT libary
+import Adafruit_DHT
+import time
+
+# sensor type
+DHT_SENSOR = Adafruit_DHT.DHT11
+
+# GPIO pin DHT11 output is going too
+GPIO_PIN = 17
+
+
+def main():
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, GPIO_PIN)
+        if humidity is not None and temperature is not None:
+            print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        else:
+            print('Failed - Recheck code')
+        time.sleep(3)
+
+
 if __name__ == "__main__":
-    print("hello world")
+    main()
+
+
+# Help Ref:
+# https://www.thegeekpub.com/236867/using-the-dht11-temperature-sensor-with-the-raspberry-pi/
