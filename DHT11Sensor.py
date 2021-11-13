347ca3c7122de6ead3f0cbc68000ba54b076b07f
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-11-13 09:09:17 +0000

    writing to a file every 20 mins to get more data

--- DHT11Sensor.py
+++ DHT11Sensor.py
@@ -34,7 +34,7 @@ def main():
         humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, GPIO_PIN)
         if humidity is not None and temperature is not None:
             print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-            if minute >= 30:  # Only for collecting data for UDP
+            if minute >= 20:  # Only for collecting data for UDP
                 minute = 0
                 write_to_file(humidity, temperature)
         else:
